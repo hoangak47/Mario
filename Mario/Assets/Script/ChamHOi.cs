9a38289d16566a37a5e6d26be0154802a64d5356
hoangak47 <57388240+hoangak47@users.noreply.github.com>
2020-11-21 01:15:30 +0700

    asa

---
+++ Mario/Assets/Script/ChamHOi.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChamHOi : MonoBehaviour
+{
+    public float DoNay = 0.5f;
+    public float TocDoNay = 4f;
+    private bool DuocNay = true;
+    private Vector3 vtDau;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player" && collision.contacts[0].normal.y > 0)
+        {
+            vtDau = transform.position;
+            KhoiNayLen();
+        }
+    }
+    void KhoiNayLen()
+    {
+        
+        StartCoroutine(KhoiNay());
+        DuocNay = false;
+    }
+    IEnumerator KhoiNay()
+    {
+        while (true)
+        {
+            transform.localPosition = new Vector2(transform.localPosition.x, transform.localPosition.y + TocDoNay * Time.deltaTime);
+            if (transform.localPosition.y >= vtDau.y + DoNay) break;
+            yield return null;
+        }
+        while(true)
+        {
+            Score.scorea += 100;
+            transform.localPosition = new Vector2(transform.localPosition.x, transform.localPosition.y - TocDoNay * Time.deltaTime);
+            if (transform.localPosition.y <= vtDau.y) break;
+            Destroy(gameObject);
+            GameObject KhoiRong = (GameObject)Instantiate(Resources.Load("Prefabs/KhoiTrong"));
+            KhoiRong.transform.position = vtDau;
+            yield return null;
+        }
+    }
+}
---
+++ Mario/Assets/Script/MarioScript.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarioScript : MonoBehaviour
+{
+    private float VanToc = 8;
+    private float TocDo = 0;
+    private float NhayCao = 450;
+    private bool DuoiDat = true;
+    private bool ChuyenHuong = false;
+    private float Down = 3;
+    private float SlowJump = 2;
+    private new Rigidbody2D rigidbody;
+
+    private bool QuayPhai = true;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        animator.SetFloat("TocDo", TocDo);
+        animator.SetBool("MatDat", DuoiDat);
+        animator.SetBool("ChuyenHuong", ChuyenHuong);
+        NhayLen();
+    }
+    private void FixedUpdate()
+    {
+        DiChuyen();
+    }
+    void DiChuyen()
+    {
+        float phimQuaLai = Input.GetAxis("Horizontal");
+        rigidbody.velocity = new Vector2(VanToc * phimQuaLai, rigidbody.velocity.y);
+        TocDo = Mathf.Abs(VanToc * phimQuaLai);
+        if(phimQuaLai>0 && !QuayPhai)
+        {
+            HuongMat();
+        }
+        if (phimQuaLai < 0 && QuayPhai)
+        {
+            HuongMat();
+        }
+    }
+    void HuongMat()
+    {
+        QuayPhai = !QuayPhai;
+        Vector2 Huong = transform.localScale;
+        Huong.x *= -1;
+        transform.localScale = Huong;
+        if (TocDo > 0) StartCoroutine(MarioChuyenHuong());
+    }
+    void NhayLen()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.W) && DuoiDat == true && transform.position.y < 2 )
+        {
+            rigidbody.AddForce((Vector2.up)* NhayCao);
+            DuoiDat = false;
+        }
+        if (rigidbody.velocity.y < 0)
+        {
+            rigidbody.velocity += Vector2.up * Physics2D.gravity.y * (Down - 1) * Time.deltaTime;
+        }
+        else if (rigidbody.velocity.y > 0 && !Input.GetKey(KeyCode.W))
+        {
+            rigidbody.velocity += Vector2.up * Physics2D.gravity.y * (SlowJump - 1) * Time.deltaTime;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "MatDat")
+        {
+            DuoiDat = true;
+        }
+    }
+    IEnumerator MarioChuyenHuong()
+    {
+        ChuyenHuong = true;
+        yield return new WaitForSeconds(0.2f);
+        ChuyenHuong = false;
+    }
+}
